d17cbad7ebbec85438cf95a4c9f3cf147970151a
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-22 17:49:19 -0400

    added form to admin main

--- pskills/src/components/AdminMain.jsx
+++ pskills/src/components/AdminMain.jsx
@@ -1,21 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {connect} from 'react-redux';
-import {fetchData} from '../actions/index';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { fetchData } from '../actions/index';
 import '../App.css'
 import styled from "styled-components";
 import { backgroundColor, cardColor, mainFont } from '../Styling';
 
-const H1 = styled.h1 `
+
+const H1 = styled.h1`
 
 `
 
-const MainDisplay = styled.div `
+const MainDisplay = styled.div`
     background: ${backgroundColor};
     height: 100vh;
     display:flex;
 `
 
-const Card = styled.div `
+const Card = styled.div`
     text-align:center;
     background: ${cardColor};
     height: 20%;
@@ -25,22 +26,46 @@ const Card = styled.div `
     border:1px solid black;
     justify-content:space-evenly;
 `
+const Form = styled.form`
+display: flex;
+flex-direction: columns;
+align-items: center;
+height 400px;
+`
 
 const initialLocation = {
     id: '',
     name: '',
     type: '',
     residents: [],
-  };
+};
 
-  
 
-const AdminMain = (props) => {  
+
+const AdminMain = (props) => {
     const [location, setLocation] = useState(initialLocation)
+
+    const [employee, setEmployee] = useState({
+        name: " ",
+        location: " ",
+        skills: ' ',
+    });
+    const [display, setDisplay] = useState(false);
+    const [displayTwo, setDisplayTwo] = useState(false);
+
+    const click = e => {
+        if (display == true) {
+            setDisplay(false);
+        }
+        if (display == false) {
+            setDisplay(true);
+        }
+    };
+
 
     const thisId = props.match.params.provider - 1
 
-    useEffect (() => {
+    useEffect(() => {
         props.fetchData();
         setLocation(props.jobData[thisId]);
         const thisLocation = props.jobData.find(
@@ -48,49 +73,98 @@ const AdminMain = (props) => {
         );
         if (thisLocation) setLocation(thisLocation);
     }, [])
-    
+
     console.log(props.jobData[thisId])
-  
+
     console.log(props.match.params.provider)
 
-    if(props.loading) {
-        
+
+    const changeHandler = e => {
+        setEmployee({ ...employee, [e.target.name]: e.target.value });
+        console.log(e.target.value);
+    };
+    const submit = e => {
+        setDisplayTwo(true);
+        e.preventDefault();
+    }
+
+    if (props.loading) {
+
         return <h3>Loading Data...</h3>
     }
-    
-        return (
-            <>
-                <H1>Data</H1>
-                <MainDisplay>
-                    
-                    {props.error && <p>{props.error}</p>}
-                        <>
-                            <Card onClick={() => props.history.push(`/admin-individual/${location.id}`)}>
-                                <p>{location.name}</p>
-                                <p>{location.type}</p>
-                                <p>{location.dimension}</p>
-                            </Card>
-                            <Card>
-                                <p>{location.name}</p>
-                                <p>{location.type}</p>
-                                <p>{location.dimension}</p>
-                            </Card>
-                            <Card>
-                                <p>{location.name}</p>
-                                <p>{location.type}</p>
-                                <p>{location.dimension}</p>
-                            </Card>
-                            <Card>
-                                <p>{location.name}</p>
-                                <p>{location.type}</p>
-                                <p>{location.dimension}</p>
-                            </Card>
-                        </>
+
+    return (
+        <>
+            <H1>Data</H1>
+            <button onClick={click}>{!display && "Add Person" || 'Hide' }</button>
+            {display && <Form onSubmit={submit}>
+                <div>
+                <label htmlFor="name">Name</label>
+                <input
+                    name="name"
+                    id="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={changeHandler}
+                    value={employee.name}
+                />
+                </div>
+                <div>
+                <label htmlFor="location">Location</label>
+                <textarea
+                    name="location"
+                    id="location"
+                    placeholder="Location"
+                    onChange={changeHandler}
+                    value={employee.location}
+                />
+                </div>
+                <div>
+                <label htmlFor="skills">Skills</label>
+                <textarea
+                    name="skills"
+                    id="skills"
+                    placeholder="Skills"
+                    onChange={changeHandler}
+                    value={employee.skills}
+                />
+                </div>
+                <button type="submit">Add Employee</button>
+            </Form>}
+            <MainDisplay>
+                {props.error && <p>{props.error}</p>}
+                <>
+                    <Card onClick={() => props.history.push(`/admin-individual/${location.id}`)}>
+                        <p>{location.name}</p>
+                        <p>{location.type}</p>
+                        <p>{location.dimension}</p>
+                    </Card>
+                    <Card>
+                        <p>{location.name}</p>
+                        <p>{location.type}</p>
+                        <p>{location.dimension}</p>
+                    </Card>
+                    <Card>
+                        <p>{location.name}</p>
+                        <p>{location.type}</p>
+                        <p>{location.dimension}</p>
+                    </Card>
+                    <Card>
+                        <p>{location.name}</p>
+                        <p>{location.type}</p>
+                        <p>{location.dimension}</p>
+                    </Card>
+                    {displayTwo && <Card >
+                        <p>Name:{employee.name}</p>
+                        <p>Location:{employee.location}</p>
+                        <p>Skills: {employee.skills}</p>
+                    </Card>}
+                </>
 
 
-                </MainDisplay>
-            </>
-        );
+            </MainDisplay>
+        </>
+    );
 }
 
 // React 2 stuff, feel free to check it out
@@ -104,4 +178,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, {fetchData})(AdminMain)
+export default connect(mapStateToProps, { fetchData })(AdminMain)
