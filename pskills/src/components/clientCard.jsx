ce4d2df7817dbbe4319975ab10b8b0cbb5b7bad2
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-24 19:37:23 -0400

    added some pictures

--- pskills/src/components/clientCard.jsx
+++ pskills/src/components/clientCard.jsx
@@ -6,22 +6,30 @@ background: rgba(255, 255, 255, 0.8);
 border-radius: 10px;
 box-shadow: 0 0 10px black;
 padding: 1rem 2.2rem;
-max-width: 100%;
+width: 100%;
 text-align: center; 
 margin-top: 1.5rem;
 transition: transform 1.5s;
 
 &:hover {
-  transform: scale(1.3);
+  transform: scale(1.2);
   margin: 5rem;
 }
+`
+const Pic = styled.img`
+max-width: 500px
+height: 500px;
+`
+const Body = styled.div`
+display:flex;
+justify-content:center;
 `
 
 export default function ClientCard(props) {
   return (
-    <div>
+    <Body>
     <Card >
-        <img variant="top" src={props.image} />
+        <Pic variant="top" src={props.image} />
           <div>
             <h2>{props.name}</h2>
             <div>
@@ -30,6 +38,6 @@ export default function ClientCard(props) {
             </div>
         </div>
       </Card>
-  </div>
+  </Body>
   )
 }
