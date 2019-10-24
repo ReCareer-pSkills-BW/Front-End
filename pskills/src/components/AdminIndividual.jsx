4aea2fba568a0e531bb60b0b947b2f7afb9d50bf
Gabriel Anguiano <anguianog97@gmail.com>
2019-10-23 19:57:38 -0700

    I was able to obtain the data and display them in the AdminIndividuals page

--- pskills/src/components/AdminIndividual.jsx
+++ pskills/src/components/AdminIndividual.jsx
@@ -2,19 +2,27 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import PrisonCard from './PrisonCard'
 import AddCandidate from './AddCandidate'
+import {NavLink} from 'react-router-dom'
 
 
 function PrisonList() {
-const [prison, setPrisonList] = useState([])
-
+const [prison, setPrisonList] = useState([])    
+const [statePen, setStatePen] = useState([])
+const [countyJail, setCountyJail] =useState([])
+const [federalFacilty, setFederalFacility] = useState([])
+const backUp = 'https://rickandmortyapi.com/api/character/'
  
     useEffect(() =>{
         const getPrisons = () => {
-        axios.get('https://rickandmortyapi.com/api/character/')
+         axios.get('http://localhost:3333/providers')
           .then( res => {
-              setPrisonList(res.data.results);
-              console.log(res)
-             
+              //setPrisonList(res.data.results);
+              const statePenCandidates = res.data[0].candidates
+              const countyJailCandidates = res.data[1].candidates
+              console.log(statePenCandidates);
+              setStatePen(statePenCandidates);
+              setCountyJail(countyJailCandidates);
+              
           })
          .catch(err => {
              console.log('Error in server', err)
@@ -22,17 +30,23 @@ const [prison, setPrisonList] = useState([])
     }
     getPrisons(); 
     }, [])
-   const editCandidate = newCandidate => {
-       setPrisonList([...prison, newCandidate])
-   }
+//    const editCandidate = newCandidate => {
+//        setPrisonList([...prison, newCandidate])
+//    }
     return (
         <div>
-       <AddCandidate editCandidate ={editCandidate}/> 
-           
-        {prison.map(value => (
-
-      <PrisonCard value ={value}/>
+     {/* <AddCandidate editCandidate ={editCandidate}/>  */}
+           {countyJail.map(value => (
+               <NavLink to = {`/admin-individual/${value.id}`}>
+               <PrisonCard candidate ={value} id ={value.id}/>
+               </NavLink>
+           ))}
+        {statePen.map(value => (
+            <PrisonCard candidate ={value} id ={value.id}/>
         ))}
+
+      
+        
         </div>
     )
 
